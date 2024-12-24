1d78f643cc217a553bebf4e8038cce6a091727a9
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-24 22:05:48 +0200

    Update Cryptocurrency.swift

--- CryptocurrencyTracking/Models/Cryptocurrency.swift
+++ CryptocurrencyTracking/Models/Cryptocurrency.swift
@@ -13,9 +13,22 @@ struct Cryptocurrency: Codable {
     let id, symbol, name: String
     let image: String
     let currentPrice: Double
-
+    let priceChangePercentage: Double
     enum CodingKeys: String, CodingKey {
         case id, symbol, name, image
         case currentPrice = "current_price"
+        case priceChangePercentage = "price_change_percentage"
+    }
+}
+extension Cryptocurrency {
+    static func dummy() -> Self {
+        Cryptocurrency(
+            id: UUID().uuidString,
+            symbol: "btc",
+            name: "Bitcoin",
+            image: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+            currentPrice: 7000,
+            priceChangePercentage: 1.5
+        )
     }
 }
