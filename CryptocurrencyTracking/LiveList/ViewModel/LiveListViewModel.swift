45d3496807c30c9e77c9603e30acaec0542d182b
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-25 18:30:00 +0200

    published properties - init

--- CryptocurrencyTracking/LiveList/ViewModel/LiveListViewModel.swift
+++ CryptocurrencyTracking/LiveList/ViewModel/LiveListViewModel.swift
@@ -7,4 +7,14 @@
 
 import Foundation
 final class LiveListViewModel: ObservableObject {
+    // MARK: - Published
+    @Published var currencies: [CurrencyPresentedModel] = CurrencyPresentedModel.generateDummyList(count: 4)
+    @Published var searchText: String = ""
+    @Published var isLoading: Bool = false
+    @Published var hasError: Bool = false
+    @Published var errorMessage: String = ""
+    private let fetchCryptocurrencyUseCase: FetchCryptocurrenciesUseCaseProtocol
+    init(fetchCryptocurrencyUseCase: FetchCryptocurrenciesUseCaseProtocol) {
+        self.fetchCryptocurrencyUseCase = fetchCryptocurrencyUseCase
+    }
 }
