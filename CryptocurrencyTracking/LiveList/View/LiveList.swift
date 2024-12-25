92e368b5e3c6162d96bd0943d54d42355ffcecd4
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-25 18:32:12 +0200

    re-structure and applying viewmodel

---
+++ CryptocurrencyTracking/LiveList/View/LiveList.swift
@@ -0,0 +1,48 @@
+//
+//  LiveList.swift
+//  CryptocurrencyTracking
+//
+//  Created by Ahmad Yasser on 24/12/2024.
+//
+
+import SwiftUI
+
+struct LiveList: View {
+    @StateObject private var viewModel: LiveListViewModel
+    init(viewModel: LiveListViewModel = DefaultViewModelFactory().makeLiveListViewModel()) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack {
+                    HStack {
+                        SearchField(searchText: $viewModel.searchText)
+                        NavigationLink(
+                            destination: FavoritesView(
+                                favorites: viewModel.currencies)
+                        ) {
+                            Image(systemName: "heart.circle.fill")
+                                .foregroundColor(.red)
+                                .imageScale(.large)
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 2)
+                    CurrencyList(currencies: viewModel.currencies)
+                        .padding()
+                        .shimmeringRedacted(active: viewModel.isLoading)
+                }
+            }
+            .onFirstAppear {
+                viewModel.viewDidLoad()
+            }
+            .toast(isShowing: $viewModel.hasError,message: viewModel.errorMessage, type: .error)
+            .navigationBarTitle("Live List", displayMode: .large)
+        }
+    }
+}
+
+#Preview {
+    LiveList()
+}
