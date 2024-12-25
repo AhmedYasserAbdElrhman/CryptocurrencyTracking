9118803918bcc651958a20950e99ff4ea20aaa0b
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-25 18:36:51 +0200

    Create ViewFirstAppearModifier.swift

---
+++ CryptocurrencyTracking/Helpers/UI/ViewFirstAppearModifier.swift
@@ -0,0 +1,30 @@
+//
+//  ViewFirstAppearModifier.swift
+//  CryptocurrencyTracking
+//
+//  Created by Ahmad Yasser on 25/12/2024.
+//
+
+import SwiftUI
+public extension View {
+    func onFirstAppear(perform action: @escaping () -> Void) -> some View {
+        modifier(ViewFirstAppearModifier(perform: action))
+    }
+}
+
+struct ViewFirstAppearModifier: ViewModifier {
+    @State private var didAppearBefore = false
+    private let action: () -> Void
+
+    init(perform action: @escaping () -> Void) {
+        self.action = action
+    }
+
+    func body(content: Content) -> some View {
+        content.onAppear {
+            guard !didAppearBefore else { return }
+            didAppearBefore = true
+            action()
+        }
+    }
+}
