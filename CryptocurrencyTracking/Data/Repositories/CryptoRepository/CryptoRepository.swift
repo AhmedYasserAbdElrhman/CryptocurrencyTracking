6b3a4436d6d3409f6d6c16399016b51fdf2f09cc
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-25 18:01:42 +0200

    create Repository

---
+++ CryptocurrencyTracking/Data/Repositories/CryptoRepository/CryptoRepository.swift
@@ -0,0 +1,22 @@
+//
+//  CryptoRepository.swift
+//  CryptocurrencyTracking
+//
+//  Created by Ahmad Yasser on 25/12/2024.
+//
+
+import Foundation
+final class CryptoRepository {
+    private let networkClient: NetworkClientProtocol
+    
+    init(networkClient: NetworkClientProtocol) {
+        self.networkClient = networkClient
+    }
+}
+// MARK: - CryptoRepositoryProtocol
+extension CryptoRepository: CryptoRepositoryProtocol {
+    func fetchCryptocurrencies(vsCurrency: String) async throws -> [Cryptocurrency] {
+        let route = NetworkRouter.fetchCryptocurrencies(vsCurrency: vsCurrency)
+        return try await networkClient.performRequest(route)
+    }
+}
---
+++ CryptocurrencyTracking/Data/Repositories/CryptoRepository/CryptoRepositoryProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  CryptoRepositoryProtocol.swift
+//  CryptocurrencyTracking
+//
+//  Created by Ahmad Yasser on 25/12/2024.
+//
+
+
+protocol CryptoRepositoryProtocol {
+    func fetchCryptocurrencies(vsCurrency: String) async throws -> [Cryptocurrency]
+}
