8187c99c28a1876d323884bae5a5058a448de88a
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-25 18:27:57 +0200

    Create LiveListViewModel.swift

---
+++ CryptocurrencyTracking/LiveList/ViewModel/LiveListViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  LiveListViewModel.swift
+//  CryptocurrencyTracking
+//
+//  Created by Ahmad Yasser on 25/12/2024.
+//
+
+import Foundation
+final class LiveListViewModel: ObservableObject {
+}
