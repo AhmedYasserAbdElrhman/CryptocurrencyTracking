a7f486e0392220ae740ada5b560444210f0f41f0
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-26 14:51:22 +0200

    move add - delete dependicy to currency list to make it reusable

--- CryptocurrencyTracking/Helpers/Factory/ViewModelFactory.swift
+++ CryptocurrencyTracking/Helpers/Factory/ViewModelFactory.swift
@@ -8,6 +8,9 @@
 
 protocol ViewModelFactory {
     func makeLiveListViewModel() -> LiveListViewModel
+    func makeCurrencyListViewModel() -> CurrencyListViewModel
+    func makeDetailViewModel(with id: String) -> CurrencyDetailsViewModel
+    func makeFavoritesViewModel() -> FavoritesViewModel
 }
 
 struct DefaultViewModelFactory: ViewModelFactory {
@@ -16,11 +19,29 @@ struct DefaultViewModelFactory: ViewModelFactory {
         let repository: CryptoRepositoryProtocol = CryptoRepository(networkClient: networkClient)
         let favoritesStorage: FavoritesRepositoryProtocol = FavoritesStorage()
         let isFavoriteUseCase: IsFavoriteUseCaseProtocol = IsFavoriteUseCase(favoritesRepository: favoritesStorage)
-        let addFavoriteUseCase: AddFavoriteUseCaseProtocol = AddFavoriteUseCase(favoritesRepository: favoritesStorage)
-        let deleteFavoriteUseCase: DeleteFavoriteUseCaseProtocol = DeleteFavoriteUseCase(favoritesRepository: favoritesStorage)
-        let useCase: FetchCryptocurrenciesUseCaseProtocol = CryptocurrenciesUseCase(repository: repository, isFavoriteUseCase: isFavoriteUseCase)
-        let viewModel: LiveListViewModel = LiveListViewModel(useCase: useCase, addFavoriteUseCase: addFavoriteUseCase, deleteFavoriteUseCase: deleteFavoriteUseCase)
+        let useCase: FetchCryptocurrenciesUseCaseProtocol = FetchCryptocurrenciesUseCase(repository: repository, isFavoriteUseCase: isFavoriteUseCase)
+        let viewModel: LiveListViewModel = LiveListViewModel(useCase: useCase)
         return viewModel
     }
-    
+    func makeCurrencyListViewModel() -> CurrencyListViewModel {
+        let favoritesStorage: FavoritesRepositoryProtocol = FavoritesStorage()
+        let addFavoriteUseCase: AddFavoriteUseCaseProtocol = AddFavoriteUseCase(favoritesRepository: favoritesStorage)
+        let deleteFavoriteUseCase: DeleteFavoriteUseCaseProtocol = DeleteFavoriteUseCase(favoritesRepository: favoritesStorage)
+        let viewModel: CurrencyListViewModel = CurrencyListViewModel(addFavoriteUseCase: addFavoriteUseCase, deleteFavoriteUseCase: deleteFavoriteUseCase)
+        return viewModel
+    }
+    func makeDetailViewModel(with id: String) -> CurrencyDetailsViewModel {
+        let networkClient: NetworkClientProtocol = NetworkClient()
+        let repository: CryptoRepositoryProtocol = CryptoRepository(networkClient: networkClient)
+        let useCase: GetCurrencyDetailsUseCaseProtocol = GetCurrencyDetailsUseCase(repository: repository)
+        let viewModel: CurrencyDetailsViewModel = CurrencyDetailsViewModel(id: id, useCase: useCase)
+        return viewModel
+    }
+
+    func makeFavoritesViewModel() -> FavoritesViewModel {
+        let repository: FavoritesRepositoryProtocol = FavoritesStorage()
+        let useCase: FetchFavoritesUseCaseProtocol = FetchFavoritesUseCase(favoritesRepository: repository)
+        let viewModel: FavoritesViewModel = FavoritesViewModel(useCase: useCase)
+        return viewModel
+    }
 }
