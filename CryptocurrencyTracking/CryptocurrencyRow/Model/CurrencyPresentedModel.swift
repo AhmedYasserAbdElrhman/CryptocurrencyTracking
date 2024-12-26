ba953260a0a62635d42e811fa7b8afc94f79b443
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2024-12-26 11:49:21 +0200

    Update CurrencyPresentedModel.swift

--- CryptocurrencyTracking/CryptocurrencyRow/Model/CurrencyPresentedModel.swift
+++ CryptocurrencyTracking/CryptocurrencyRow/Model/CurrencyPresentedModel.swift
@@ -13,13 +13,24 @@ struct CurrencyPresentedModel: Identifiable, Equatable {
     let imageURL: URL?
     let formattedPrice: String
     let priceChange: String?
-    init(cryptocurrency: Cryptocurrency) {
+    var isFavorite: Bool
+    init(cryptocurrency: Cryptocurrency, isFavorite: Bool = false) {
         self.id = cryptocurrency.id
         self.symbol = cryptocurrency.symbol.uppercased()
         self.name = cryptocurrency.name
         self.imageURL = URL(string: cryptocurrency.image)
         self.formattedPrice = String(format: "$%.2f", cryptocurrency.currentPrice)
         self.priceChange = cryptocurrency.priceChangePercentage?.formatted(.percent)
+        self.isFavorite = isFavorite
+    }
+    init(favorite: FavoriteCurrency) {
+        self.id = favorite.id ?? ""
+        self.symbol = favorite.symbol ?? ""
+        self.name = favorite.name ?? ""
+        self.imageURL = favorite.imageURL
+        self.formattedPrice = favorite.formattedPrice ?? ""
+        self.priceChange = favorite.priceChange
+        self.isFavorite = true
     }
 }
 extension CurrencyPresentedModel {
